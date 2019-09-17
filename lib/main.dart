735876377a7124fe64917e035ce4aba591870b55
Osama Khan <osamakhn@gmail.com>
2019-09-16 21:38:48 -0700

    layouts-exercise

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,50 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Row(
+            // mainAxisSize: MainAxisSize.min,
+            // verticalDirection: VerticalDirection.up,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Container(
+                  // height: 100,
+                  width: 100,
+                  color: Colors.blue,
+                  child: Text('Container 1')),
+              // SizedBox(height: 20, width: 10),
+              Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+                Container(
+                  color: Colors.yellow,
+                  height: 100,
+                  width: 100,
+                ),
+                Container(
+                  color: Colors.green,
+                  height: 100,
+                  width: 100,
+                ),
+              ]),
+              // SizedBox(height: 20, width: 10),
+              Container(
+                  // height: 100,
+                  width: 100,
+                  color: Colors.pink,
+                  child: Text('Container 3'))
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
